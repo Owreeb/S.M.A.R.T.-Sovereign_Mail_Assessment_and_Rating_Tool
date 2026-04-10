1b738d5d255dbdf7cb25385abdab71896495b721
Oliver Reeb <180310506+Owreeb@users.noreply.github.com>
2026-04-10 09:56:53 +0200

    initial structure

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+
+import babel from '@rolldown/plugin-babel'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
+})
